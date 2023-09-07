dd9eb6f47b8defef310401b886184158891be357
Lee <jjn3912@naver.com>
2023-09-07 16:15:53 +0900

    week02 v0.5

--- week02_numpy.py
+++ week02_numpy.py
@@ -5,8 +5,6 @@ import random
 #v= np.array([[1, 3, -9, 2],[71, 13, -22, 7]])#, dtype='int64')
 #print(v.ndim, v.shape, v.data, v.dtype, v.strides)
 n = int(input("input number :"))
-l = list()
-for i in range(n):
-    l.append(random.randint(1,100))
+l = [random.randint(1,100) for i in range(n)]
 v= np.array(l, dtype='int16')
 print(v)
