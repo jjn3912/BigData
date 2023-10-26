7351191660d9fd2a5b335592872e42e9adc24eb9
Lee <jjn3912@naver.com>
2023-10-26 15:11:19 +0900

    week09_numpy_v0.8

---
+++ week09_numpy.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+np_1d = np.arange(1,10,2)
+print(np_1d, type(np_1d))
+print(np_1d, type(np_1d[0]))
+print(np_1d.ndim)
+print(np_1d.size)
+print(np_1d.shape)
+# any_list = [91, "university",3.91,(5, 7)]
+# converted_any_list = ["1", "university","3.91","(5, 7)"]
+#
+# # one_type_np_list = np.array(any_list)
+# one_type_np_list = np.array([2, 4, 6, 8])
+# np_1d = np.array([2,4,6,8])
+# np_2d = np.array([[2,4],[6,8]])
+# print(np_1d.ndim, np_2d.ndim)
+# print(np_1d.size,np_2d.size)
+# print(np_1d.shape,np_2d.shape)
+# # print(one_type_np_list)
+# # for i in any_list:
+# #     print(i)
+# #
+# # for j in one_type_np_list:
+# #     print(j)
