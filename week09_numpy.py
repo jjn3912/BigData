fbf0d81d4c6ea3836906cc784cc95eaf04c2d062
Lee <jjn3912@naver.com>
2023-10-26 15:44:03 +0900

    week09_numpy v4.0

--- week09_numpy.py
+++ week09_numpy.py
@@ -1,14 +1,33 @@
 import numpy as np
-import random
-def info(x):
-    print(f"배열의 차원은{x.ndim}입니다")
-    print(f"배열의 Shape는{x.shape}입니다")
-    print(f"배열 원소의 개수는{x.size}개 입니다")
+import pandas as pd
+from sklearn.impute import SimpleImputer
+i = SimpleImputer(strategy='mean')
 
-np_1d = np.arange(1,20,2)
-np_1d = np_1d.reshape(1,2,5)
-print(np_1d)
-info(np_1d)
+df = pd.DataFrame({
+    'A' : [1, 2, np.nan, 4],
+    'B' : [5, np.nan, 7, 8],
+    'C' : [9, 10,11, np.nan]
+})
+print(df)
+df =pd.DataFrame(i.fit_transform(df), columns=df.columns)
+print(df)
+# for col in df.columns:
+#     df[col] = np.where(pd.isnull(df[col]),np.mean(df[col]),df[col])
+#     print(df)
+
+# print(df)
+# df.fillna(df.mean(), inplace=True)
+# print(df)
+
+# def info(x):
+#     print(f"배열의 차원은{x.ndim}입니다")
+#     print(f"배열의 Shape는{x.shape}입니다")
+#     print(f"배열 원소의 개수는{x.size}개 입니다")
+#
+# np_1d = np.arange(1,20,2)
+# np_1d = np_1d.reshape(1,2,5)
+# print(np_1d)
+# info(np_1d)
 
 # any_list = [91, "university",3.91,(5, 7)]
 # converted_any_list = ["1", "university","3.91","(5, 7)"]
