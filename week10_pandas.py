f006ef7fc11f7c276ccf02a19f7d190b41583560
Lee <jjn3912@naver.com>
2023-11-02 15:37:55 +0900

    week10 v4.6 numpy & pandas

--- week10_pandas.py
+++ week10_pandas.py
@@ -1,11 +1,29 @@
 import numpy as np
-
-a1 = np.array([[1, 2],
-               [3, 2],
-                    ])
-a2 = np.array([[2, 1],
-               [5, 0],
-               ])
-#print(np.dot(a1, a2))
-#print(a1@a2)
-print(np.matmul(a1,a2))
+# a0 = np.ones((3, 3))
+# a1 = a0[:,2] + 1
+# print(a1.shape)
+# a1 = np.array([[1, 1, 2],
+#               [1, 1, 2],
+#               [1, 1, 2] ])
+# a2 = np.array([[1, 0, 0],
+#               [2, 0, 0],
+#               [1, 0, 0]
+#               ])
+a1 = np.ones((3, 3))
+a1[:, 2] =2
+print(a1)
+a2 = np.full((3,3), 5)
+print(a2)
+a3 = np.eye(3)
+print(a3)
+a4 = a3[:2, 1:]
+print(a4)
+a5 = a3[2]
+# a5 = a3[2, :]
+# a5 = a3[2:, :]
+print(a5)
+# a6 = a3[1,:2]
+a6 = a3[1:2,:2]
+print(a6)
+a7 = a3[:,:2]
+print(a7)
