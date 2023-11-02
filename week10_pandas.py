7389522f6f88e7e39e7e89685010a222820cd940
Lee <jjn3912@naver.com>
2023-11-02 16:23:56 +0900

    week10 v4.9 numpy pandas

--- week10_pandas.py
+++ week10_pandas.py
@@ -1,22 +1,14 @@
 import numpy as np
-a1 = np.arange(1, 6)
-a2 = np.arange(6,11)
-a3 = np.array([
-    [3, 2, 1],
-    [-11, 0, 9]
+from numpy.linalg import inv
+#linalg == linear algebra
+np.random.seed()
+X = np.array([
+    [1,2,3],
+    [1,0,0],
+    [0,0,1]
 ])
-print(a1 + a2)
-print(a1 - a2)
-print(a1 * a2)
-print(a1 / a2)
-print(np.dot(a1, a2))
-# a3 = a1 + a2
-# print(a3)
-print(np.sum(a1) + np.sum(a2))
-print(np.prod(a1), np.prod(a2))
-print(a3.sum(axis=0))
-print(a3.sum(axis=1))
-print(a3.prod(axis=0))
-print(a3.prod(axis=1))
-print(a3.mean())
-print(a3.std())
+print(X)
+Z =inv(X) #역행렬
+print(Z)
+
+print(X.dot(Z))
