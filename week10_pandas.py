e415c470e49e56681d79e9f80df78feb16f3535e
Lee <jjn3912@naver.com>
2023-11-02 15:49:27 +0900

    week10 v4.7 numpy pandas

--- week10_pandas.py
+++ week10_pandas.py
@@ -1,29 +1,22 @@
 import numpy as np
-# a0 = np.ones((3, 3))
-# a1 = a0[:,2] + 1
-# print(a1.shape)
-# a1 = np.array([[1, 1, 2],
-#               [1, 1, 2],
-#               [1, 1, 2] ])
-# a2 = np.array([[1, 0, 0],
-#               [2, 0, 0],
-#               [1, 0, 0]
-#               ])
-a1 = np.ones((3, 3))
-a1[:, 2] =2
-print(a1)
-a2 = np.full((3,3), 5)
-print(a2)
-a3 = np.eye(3)
-print(a3)
-a4 = a3[:2, 1:]
-print(a4)
-a5 = a3[2]
-# a5 = a3[2, :]
-# a5 = a3[2:, :]
-print(a5)
-# a6 = a3[1,:2]
-a6 = a3[1:2,:2]
-print(a6)
-a7 = a3[:,:2]
-print(a7)
+a1 = np.arange(1, 6)
+a2 = np.arange(6,11)
+a3 = np.array([
+    [3, 2, 1],
+    [-11, 0, 9]
+])
+print(a1 + a2)
+print(a1 - a2)
+print(a1 * a2)
+print(a1 / a2)
+print(np.dot(a1, a2))
+# a3 = a1 + a2
+# print(a3)
+print(np.sum(a1) + np.sum(a2))
+print(np.prod(a1), np.prod(a2))
+print(a3.sum(axis=0))
+print(a3.sum(axis=1))
+print(a3.prod(axis=0))
+print(a3.prod(axis=1))
+print(a3.mean())
+print(a3.std())
