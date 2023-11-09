68e46434212c4cb2f13425342b61ca4de01fa2d7
Lee <jjn3912@naver.com>
2023-11-09 16:05:07 +0900

    week11 v5.7 numpy & pandas

--- week11_pandas_review.py
+++ week11_pandas_review.py
@@ -3,14 +3,20 @@ import pandas as pd
 
 df = pd.DataFrame(
     [
-        [99, 89, 100],
+        [99, 89, 81],
         [91, 98, 90],
-        [100, 97, 85],
-        [83, 97, 94]
+        [95, 97, 85],
+        [83, 96, 94]
     ], index=[1, 2, 3, 4],
     columns=["KOR","ENG","MAT"]
 
 )
-print(df)
+print(df.mean())
+print(df.max())
 
 print(df.query('KOR>=95 and ENG>=95'))
+print(df.at[1,'MAT'])
+def scale_score(n):
+    return n + 1
+df = df.apply(scale_score)
+print(df)
