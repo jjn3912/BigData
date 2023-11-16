3ce7b2ef2a81f8bf82e19d7096c222d5cb3284e7
Lee <jjn3912@naver.com>
2023-11-16 15:22:13 +0900

    week12 v6.4 web

--- week12_web03.py
+++ week12_web03.py
@@ -6,16 +6,14 @@ html = """
 <title>스크레이핑 실습</title>
 </head>
 <body>
-<h1 id="univ">대림대학교</h1>
-<p>웹스크레이핑</p>
-<p id="contents">파이썬, 판다스, 넘파이, 맷플롯립, GUI...</p>
+<a href="https://www.daelim.ac.kr">대림대학교</a><br>
+<a href="http://www.harvard.edu">하버드대학교</a><br>
 </body>
 </html>
 """
 soup = BeautifulSoup(html, 'html.parser')
-university = soup.find(id='univ')
-contents = soup.find(id='contents')
-
-print(university)
-print(contents.string)
-print(contents)
+urls = soup.find_all('a')
+print(urls)
+#하버드대학교 rl주소는 http://www.harvard.edu 입니다.
+for url in urls:
+    print("{0}의 주소는 {1}입니다.".format(url.string, url.attrs['href']))
