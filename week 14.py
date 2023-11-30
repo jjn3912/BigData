8dd85de9c56f9575d56e2a70df7a2c4499992c29
Lee <jjn3912@naver.com>
2023-11-30 15:03:36 +0900

    week 14 v7.0

--- week 14.py
+++ week 14.py
@@ -2,14 +2,15 @@ import seaborn as sns
 import pandas as pd
 
 titanic = sns.load_dataset('titanic')
-#print(titanic.describe())
-#print(titanic.head(10))
-#print(titanic.tail(10))
 
-# 결측치 전처리
-print(titanic['age'].fillna(titanic['age'].median(), inplace=True))
-titanic['deck'] = titanic['deck'].cat.add_categories('Unknown')  # 카테고리 추가
-titanic['deck'].fillna('Unknown', inplace=True)
-titanic['embarked'].fillna('Unknown', inplace=True)
-titanic['embark_town'].fillna('Unknown', inplace=True)
-print(titanic.info())
+survived_human = titanic[titanic["survived"] == 1]["survived"].count()
+dead_human = titanic[titanic["survived"] == 0]["survived"].count()
+male_survived =  titanic[(titanic["survived"] == 1 )&(titanic["sex"] == 'male')]["survived"].count()
+female_survived =  titanic[(titanic["survived"] == 1 )&(titanic["sex"] == 'female')]["survived"].count()
+male_count =  titanic[titanic["sex"] == 'male']["sex"].count()
+female_count =  titanic[titanic["sex"] == 'female']["sex"].count()
+print(f"생존자 수 :{survived_human}명")
+print(f"사망자 수 :{dead_human}명")
+print(male_count,female_count)
+print(male_survived/male_count,female_survived/female_count)
+
