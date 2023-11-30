b86483ee999c84000503ddf02f55f6ee9d645778
Lee <jjn3912@naver.com>
2023-11-30 16:01:20 +0900

    week 14 v7.3

--- week 14.py
+++ week 14.py
@@ -1,9 +1,14 @@
 import seaborn as sns
 import pandas as pd
-
+import matplotlib.pyplot as plt
 titanic = sns.load_dataset('titanic')
 
-age_survived = titanic.groupby(pd.cut(titanic['age'],list(range(0, 81, 10))))['survived'].sum()
-print(age_survived)
+survived_ages = titanic[titanic['survived'] == 1]['age'].dropna()
+dead_ages = titanic[titanic['survived'] == 0]['age'].dropna()
 
-
+plt.hist(survived_ages, 20, label='survived',alpha = 0.5)
+plt.hist(dead_ages, 20, label = 'dead',alpha = 0.5)
+plt.xlabel('age')
+plt.ylabel('Count')
+plt.legend()
+plt.show()
